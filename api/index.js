78b8daacab6acc9e26363ce91d526d9858b8379c
izzet Onal <izzetonal46@gmail.com>
2022-11-19 20:48:53 +0100

    Node.js MySQL Route Controller Structure

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import postRoutes from './routes/posts.js'
+import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
+
+const app = express();
+
+app.use(express.json())
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+
+
+
+app.listen(8800,()=>{
+    console.log('connected!')
+})
